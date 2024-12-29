cc65c0e6ff3224a3024e46f2e219fc95900f7464
mohya@daum.net <0529456!Po>
2024-12-29 17:34:42 +0900

    feat: update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
 const prefix =
   process.env.NODE_ENV === "production" ? "https://mabihowmuch.site/" : "";
-
 const nextConfig = {
   output: "export",
   assetPrefix: prefix,
 };
+
+module.exports = nextConfig;
